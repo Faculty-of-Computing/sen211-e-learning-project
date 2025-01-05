8d809aeb9cef61a2ec10b301a3d02bb8b8bf2c23
SpiDher <penivera655@gmail.com>
2025-01-05 22:44:40 +0100

    Final touch

--- modules/COS111/scripts/API.js
+++ modules/COS111/scripts/API.js
@@ -8,6 +8,8 @@ async function fetchCourseContent(contentToFetch) {
 
 // loading indicator
   document.getElementById('loadingIndicator').style.display = 'flex';
+  document.getElementById('loading-icon').style.display='flex'
+  document.getElementById('error').style.display='none'
 
     try {
       const response = await fetch('https://cos1.vercel.app/api/content');
@@ -18,21 +20,32 @@ async function fetchCourseContent(contentToFetch) {
         // const keys = Object.keys(courseData);
         // console.log(data);
         // console.log(response);
+        document.getElementById('loading-icon').style.display='flex'
+
         console.log('Course Content:', data[contentToFetch].content);
         localStorage.setItem("Content", JSON.stringify(data[contentToFetch].content));
 
          // Hid the loading indicator once the content is fetched
          document.getElementById('loadingIndicator').style.display = 'none';
 
-         window.location.href = "../COS111/lessons/details.html";
-        //window.location.href= "./lessons/details.html";
+        // window.location.href = "../../lessons/details.html";
+        window.location.href= "../COS111/lessons/details.html";
       } else {
         console.error('Failed to fetch course content:', await response.json());
+        document.getElementById('loading-icon').style.display='none'
+
+                 document.getElementById('loadingIndicator').style.display = 'flex';
+                 document.getElementById('error').style.display='flex'
+
 
       }
     } catch (error) {
       console.error('Error:', error);
-      document.getElementById('loadingIndicator').style.display = 'none';
+      document.getElementById('loadingIndicator').style.display = 'flex';
+      document.getElementById('loading-icon').style.display='none'
+
+      document.getElementById('error').style.display='flex'
+
 
     }
   }
