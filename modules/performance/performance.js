0fa6d569e3b65b4f354aef106147e6f98964889f
Mikel Edwin <michealanthonyxxx@gmail.com>
2025-01-07 12:32:36 +0100

    Updates for performance page

--- modules/performance/performance.js
+++ modules/performance/performance.js
@@ -1,8 +1,27 @@
 // @ts-check
-import { getUser, getUserResults } from "../../scripts/firebase.js";
-import "../../lib/chart.js";
+import { getUser, getUserResults } from "/scripts/firebase.js";
+import "/lib/chart.js";
+
+/** @param {number} duration  */
+function calcDuration(duration) {
+  return `${Math.floor(duration / 60)
+    .toString()
+    .padStart(2, "0")}:${(duration % 60).toString().padStart(2, "0")}`;
+}
+/** @param {string} className  */
+function unhide(className) {
+  const loader = document.getElementById("loader");
+  if (loader) loader.remove();
+  /** @type {NodeListOf<HTMLElement>} */ (
+    document.querySelectorAll(className)
+  ).forEach((el) => {
+    el.classList.remove(className.slice(1));
+  });
+}
 
 const url = new URL(location.href);
+
+const resultsPromise = getUserResults();
 
 /** @type {string} */
 let idFromResult;
@@ -14,64 +33,89 @@ if (
   url.searchParams.has("duration") ||
   url.searchParams.has("course")
 ) {
-  const performanceTableContainer =
-    document.getElementById("performance-table");
+  idFromResult = /** @type {string} */ (url.searchParams.get("id"));
+  drawPerformanceTable({
+    score: +(/** @type {string} */ (url.searchParams.get("score"))),
+    total: +(/** @type {string} */ (url.searchParams.get("total"))),
+    duration: +(/** @type {string} */ (url.searchParams.get("duration"))),
+    course: /** @type {string} */ (url.searchParams.get("course")),
+  });
+} else {
+  resultsPromise.then((results) => {
+    const data = results.reduce(
+      (result, current) => {
+        // score, total, duration, course
+        result.score += current.score;
+        result.total += current.total;
+        result.duration += current.duration;
+        return result;
+      },
+      {
+        score: 0,
+        duration: 0,
+        total: 0,
+        course: "Learnify",
+      }
+    );
+    drawPerformanceTable(data);
+  });
+}
+
+/** @param {Result} param0 */
+async function drawPerformanceTable({ course, score, duration, total }) {
+  const performanceTableContainer = /** @type {HTMLDivElement} */ (
+    document.getElementById("performance-table")
+  );
+  unhide(".unhide-table");
   performanceTableContainer.style.display = "";
-  const scoreFromResult = url.searchParams.get("score");
-  const totalFromResult = url.searchParams.get("total");
-  idFromResult = url.searchParams.get("id");
-  const durationFromResult = url.searchParams.get("duration");
-  const courseFromResult = url.searchParams.get("course");
-
-  const average = ((+scoreFromResult / +totalFromResult) * 100).toFixed();
-
+  const average = ((score / total) * 100).toFixed();
   const user = await getUser();
-
+  const timeTaken = calcDuration(duration);
   performanceTableContainer.innerHTML = `
-    <h4 class="font-afacad text-[48px] font-bold leading-[35px] tracking-[-0.03em] text-left  text-primary">
-      ${user.displayName}
-    </h4>
-    <table id="performance">
-      <tbody>
-        <tr>
-          <td>Your score</td>
-          <td>
-            ${scoreFromResult}/${totalFromResult}
-          </td>
-        </tr>
-        <tr>
-          <td>Your average</td>
-          <td>${average}%</td>
-        </tr>
-        <tr>
-          <td>Duration</td>
-          <td>${durationFromResult}</td>
-        </tr>
-        <tr>
-          <td>Course</td>
-          <td>${courseFromResult}</td>
-        </tr>
-        <tr>
-          <td>Questions</td>
-          <td>${totalFromResult}</td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="space-x-[15px]">
-      <span class="font-afacad font-semibold text-[24px] leading-[32px] text-primary">
-        Quiz Evaluation:
-      </span>
-      <span class="font-afacad font-medium text-[24px] leading-[32px] tracking-[-0.02em] text-white">
-        Attempted: ${totalFromResult}
-      </span>
-      <span class="font-afacad font-medium text-[24px] leading-[32px] tracking-[-0.02em] text-white">
-        Passed: ${scoreFromResult}
-      </span>
-      <span class="font-afacad font-medium text-[24px] leading-[32px] tracking-[-0.02em] text-white">
-        Failed: ${+totalFromResult - +scoreFromResult}
-      </span>
-    </div>
-    `;
+      <h4 class="font-afacad text-[48px] font-bold leading-[35px] tracking-[-0.03em] text-left  text-primary">
+        ${user.displayName}
+      </h4>
+      <table id="performance">
+        <tbody>
+          <tr>
+            <td>Your score</td>
+            <td>
+              ${score}/${total}
+            </td>
+          </tr>
+          <tr>
+            <td>Your average</td>
+            <td>${average}%</td>
+          </tr>
+          <tr>
+            <td>Duration</td>
+            <td>${timeTaken}</td>
+          </tr>
+          <tr>
+            <td>Course</td>
+            <td>${course}</td>
+          </tr>
+          <tr>
+            <td>Questions</td>
+            <td>${total}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="space-x-[15px]">
+        <span class="font-afacad font-semibold text-[24px] leading-[32px] text-primary">
+          Quiz Evaluation:
+        </span>
+        <span class="font-afacad font-medium text-[24px] leading-[32px] tracking-[-0.02em] text-white">
+          Attempted: ${total}
+        </span>
+        <span class="font-afacad font-medium text-[24px] leading-[32px] tracking-[-0.02em] text-white">
+          Passed: ${score}
+        </span>
+        <span class="font-afacad font-medium text-[24px] leading-[32px] tracking-[-0.02em] text-white">
+          Failed: ${+total - score}
+        </span>
+      </div>
+      `;
 
   /**
    * @typedef {object} StatCardProps
@@ -104,19 +148,21 @@ if (
       });
     });
     return `
-        <div class="size-[251px] box-border flex items-center justify-center p-[21px_15px] gap-[32px] bg-[#1D1F21] xl:bg-transparent rounded-[20px] relative">
-          <span class="absolute font-afacad font-semibold text-[24px] leading-[32px] text-white top-3 left-0 right-0 w-full text-center inline-block">
-            ${data.title}
-          </span>
-          <div class="size-[125px] relative before:content-[''] before:size-full before:bg-black before:opacity-30 before:border-[2px_solid_black] before:rounded-full before:absolute before:-z-10 z-0 font-afacad text-semibold text-white flex justify-center items-center">
-            <canvas id="${id}" class="-mt-2.5"></canvas>
-            <span class="absolute capitalize text-lg">${data.content}</span>
+          <div class="size-[251px] box-border flex items-center justify-center p-[21px_15px] gap-[32px] bg-[#1D1F21] xl:bg-transparent rounded-[20px] relative">
+            <span class="absolute font-afacad font-semibold text-[24px] leading-[32px] text-white top-3 left-0 right-0 w-full text-center inline-block">
+              ${data.title}
+            </span>
+            <div class="size-[125px] relative before:content-[''] before:size-full before:bg-black before:opacity-30 before:border-[2px_solid_black] before:rounded-full before:absolute before:-z-10 z-0 font-afacad text-semibold text-white flex justify-center items-center">
+              <canvas id="${id}" class="-mt-2.5"></canvas>
+              <span class="absolute capitalize text-lg">${data.content}</span>
+            </div>
           </div>
-        </div>
-        `;
+          `;
   }
 
-  const startCardContainer = document.getElementById("stat-cards");
+  const startCardContainer = /** @type {HTMLDivElement} */ (
+    document.getElementById("stat-cards")
+  );
   startCardContainer.innerHTML += [
     startCard({
       content: `${average}%`,
@@ -125,39 +171,42 @@ if (
       value: +average,
     }),
     startCard({
-      content: `${scoreFromResult}/${totalFromResult}`,
+      content: `${score}/${total}`,
       title: "Score",
-      max: +totalFromResult,
-      value: +scoreFromResult,
+      max: +total,
+      value: +score,
     }),
     startCard({
-      content: durationFromResult,
+      // FIXME
+      content: calcDuration(duration),
       title: "Duration",
     }),
   ].join(" ");
 }
 
-const results = await getUserResults();
-const rows = results.map((result) => {
-  if (result.id === idFromResult) return "";
-  const average = (result.score / result.total) * 100;
-  const time = result.time.toDate();
+const results = await resultsPromise;
+const rows = results
+  .sort((a, b) => b.time.toDate().getTime() - a.time.toDate().getTime())
+  .map((result) => {
+    if (result.id === idFromResult) return "";
+    const average = (result.score / result.total) * 100;
+    const time = result.time.toDate();
 
-  /** @type {string} */
-  let className,
     /** @type {string} */
-    text;
-  if (+average >= 80) {
-    className = "text-[#05C612]";
-    text = "Great";
-  } else if (+average >= 50) {
-    className = "text-[#EFE009]";
-    text = "Good";
-  } else {
-    className = "text-[#C61805]";
-    text = "Poor";
-  }
-  return `
+    let className,
+      /** @type {string} */
+      text;
+    if (+average >= 80) {
+      className = "text-[#05C612]";
+      text = "Great";
+    } else if (+average >= 50) {
+      className = "text-[#EFE009]";
+      text = "Good";
+    } else {
+      className = "text-[#C61805]";
+      text = "Poor";
+    }
+    return `
   <span>
   ${time.getDate().toString().padStart(2, "0")} / 
   ${(time.getMonth() + 1).toString().padStart(2, "0")} /
@@ -169,10 +218,12 @@ const rows = results.map((result) => {
     ${text}
   </span>
 `;
-});
+  });
 
 if (rows.length) {
-  const historyFieldContainer = document.getElementById("history-fields");
+  unhide(".unhide-history");
+  const historyFieldContainer = /** @type {HTMLDivElement} */ (
+    document.getElementById("history-fields")
+  );
   historyFieldContainer.innerHTML = rows.join(" ");
-  historyFieldContainer.style.display = "";
 }
